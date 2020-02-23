3f03177b6c7b6b1afd22cde4f03ad0d6ed9eac99
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-23 10:32:42 -0500

    Add files via upload

---
+++ tweet_daily_list.py
@@ -0,0 +1,35 @@
+import tweepy
+from datetime import datetime, date
+
+''' This part of the module creates text lists of
+    each day's tweets and files them in separate
+    text documents '''
+
+def tweetCall(userHandle):
+  consumer_key = '***'
+  consumer_secret = '***'
+  access_token = '***'
+  access_token_secret = '***'
+
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
+  auth.set_access_token(access_token, access_token_secret)   
+  api = tweepy.API(auth)
+
+  tweets = api.user_timeline(screen_name = userHandle, count = 20, include_rts = False)
+  tweetList = []
+
+  currentDay = date.today()
+
+  for status in tweets:
+    dateString = status.created_at.strftime("%m%d%Y")
+    dateVal = datetime.strptime(dateString, "%m%d%Y").date()
+    if dateVal == currentDay:
+      tweetList.append(status.text)
+    print(dateVal)
+
+  print(currentDay)
+
+  return tweetList
+
+if __name__ == '__main__':
+  print(tweetCall('janeengbu'))
