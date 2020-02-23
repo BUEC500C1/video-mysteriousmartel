20717de984b856bd0fb9a7ba19a9fd8db5f97182
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-23 01:25:10 -0500

    Add files via upload

---
+++ word_cloud_generator.py
@@ -0,0 +1,29 @@
+import os
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+'''For the image generation, a word cloud is made for each
+   set of tweets per day. Every day will have one word cloud,
+   and there will be seven images for the week.'''
+
+def cloudGenerate(fileName):
+
+  # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+  directory = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+  # Read the whole text
+  text = open(path.join(directory, fileName)).read()
+
+  # Generate a word cloud image
+  wordcloud = WordCloud().generate(text)
+
+  # Display the generated image:
+  # the matplotlib way:
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis("off")
+  plt.show()
+  wordcloud.to_file('lines_read.png')
+
+# if __name__ == "__main__":
+#   cloudGenerate('constitution.txt')
