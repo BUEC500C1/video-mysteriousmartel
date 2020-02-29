f77423151b6d27683e1b9cc954d3815f66b1efce
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-28 22:32:57 -0500

    Add files via upload

--- tweet_queue.py
+++ tweet_queue.py
@@ -1,18 +1,30 @@
 from queue import Queue
 import threading
-import time
 
-userQueue = Queue(maxsize = 4)
+# The queue handles the inputs to the module, sending in
+# threads to be processed asynchronously and leaving the user open
+# to attend to other tasks
 
-def twitterQueueUp(userHandle):
+userQueue = Queue(maxsize = 10)
+
+def twitterQueueUp(userHandle,cycleCount):
+  handleCycles = [userHandle, cycleCount]
+  userQueue.put_nowait(handleCycles)
   queueMove()
 
-  if not userQueue.full():
-    userQueue.put_nowait(userHandle)
-    print(userQueue)
-
 def queueMove():
-  return 'hi'
+  if userQueue.empty() is True:
+    print('No handles in queue: ending process.')
+    pass
 
-if __name__ == '__main__':
-  twitterQueueUp('potato')
+  user = userQueue.get()
+
+  print('Currently processing for ' + str(user[0]))
+
+def twitterThread(userHandle):
+  pass
+  
+if __name__ == "__main__":
+    twitterQueueUp('potato',3)
+    # x = threading.Thread(target=twitterThread, args=("potato",))
+    # x.start()
