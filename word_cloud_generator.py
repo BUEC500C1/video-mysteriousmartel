c472eaa41d2e30f9dcab90df4ecf5784eb0e97c6
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-28 21:10:01 -0500

    Add files via upload

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -2,27 +2,41 @@ import os
 from os import path
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
 
 '''For the image generation, a word cloud is made for each
    tweet per day'''
 
-def cloudGenerate(fileName):
+def cloudGenerate(statusList,userHandle,dateRange,indexCount):
 
   # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-  directory = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+  # directory = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+  if not os.path.isdir(userHandle + '_DailyTweets'):
+    os.mkdir(userHandle + '_DailyTweets')
 
-  # Read the whole text
-  text = open(path.join(directory, fileName)).read()
+  tweetPath = os.getcwd() + '/' + userHandle + '_DailyTweets/'
+
+  text = ' '.join([str(tweet) for tweet in statusList])
 
   # Generate a word cloud image
   wordcloud = WordCloud().generate(text)
 
-  # Display the generated image:
-  # the matplotlib way:
-  plt.imshow(wordcloud, interpolation='bilinear')
-  plt.axis("off")
-  plt.show()
-  wordcloud.to_file('lines_read.png')
+  imageName = tweetPath + "tweet" + str(indexCount) + ".png"
 
-# if __name__ == "__main__":
-#   cloudGenerate('constitution.txt')
+  wordcloud.to_file(imageName)
+
+  # Write dates of the tweet cloud in top corner of image
+
+  # NOTE: you will need the arial.ttf font in the directory
+  # where you are running this file (available in repo)
+
+  image = Image.open(imageName)
+  write = ImageDraw.Draw(image)
+
+  font = ImageFont.truetype('arial.ttf', size=15)
+  (x,y) = (10,10)
+  date = dateRange
+  fontcolor = 'rgb(255,255,255)'
+  write.text((x,y),date,fill=fontcolor,font=font)
+
+  image.save(imageName)
