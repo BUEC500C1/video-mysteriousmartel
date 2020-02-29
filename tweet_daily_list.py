908759940948c02798f2a2b67bd9f5ac4c5993f1
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-28 21:11:57 -0500

    Add files via upload

--- tweet_daily_list.py
+++ tweet_daily_list.py
@@ -1,35 +1,32 @@
 import tweepy
+import configparser
 from datetime import datetime, date
 
 ''' This part of the module creates text lists of
-    each day's tweets and files them in separate
-    text documents '''
+    each cycle's tweets to be sent for image processing '''
 
 def tweetCall(userHandle):
-  consumer_key = '***'
-  consumer_secret = '***'
-  access_token = '***'
-  access_token_secret = '***'
-
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
-  auth.set_access_token(access_token, access_token_secret)   
+  config = configparser.ConfigParser()
+  config.read('keys')
+  auth = tweepy.OAuthHandler(config.get('auth', 'consumer_key').strip(),
+    config.get('auth', 'consumer_secret').strip())
+  auth.set_access_token(config.get('auth', 'access_token').strip(),
+    config.get('auth', 'access_secret').strip())
   api = tweepy.API(auth)
 
-  tweets = api.user_timeline(screen_name = userHandle, count = 20, include_rts = False)
+  tweets = api.user_timeline(screen_name = userHandle, count = 30, include_rts = False)
   tweetList = []
 
-  currentDay = date.today()
+  startTime = tweets[0].created_at.strftime("%m%d%Y")
+  startDate = datetime.strptime(startTime, "%m%d%Y").date()
+  startDateStr = str(startDate)
+
+  endTime = tweets[-1].created_at.strftime("%m%d%Y")
+  endDate = datetime.strptime(endTime, "%m%d%Y").date()
+  endDateStr = str(endDate)
+
+  dates = startDateStr + " to " + endDateStr
 
   for status in tweets:
-    dateString = status.created_at.strftime("%m%d%Y")
-    dateVal = datetime.strptime(dateString, "%m%d%Y").date()
-    if dateVal == currentDay:
-      tweetList.append(status.text)
-    print(dateVal)
-
-  print(currentDay)
-
-  return tweetList
-
-# if __name__ == '__main__':
-  # print(tweetCall('username'))
+    tweetList.append(status.text)
+  return tweetList, dates
